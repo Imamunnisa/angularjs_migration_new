79e43b81ac3046c571537573ae623a83cdf6820b
Imamunnisa <imamunnisa1999@gmail.com>
2021-03-02 11:34:16 +0530

    Step-3

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    mode: 'development',
+    entry: "./src/app/main.ts",
+    output: {
+        filename: "bundle.js"
+    },
+    resolve: {
+        // Add '.ts' and '.tsx' as a resolvable extension.
+        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    },
+    module: {
+        rules: [
+            // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
+            { test: /\.tsx?$/, loader: "ts-loader" }
+        ]
+    },
+    plugins: [
+        new webpack.ContextReplacementPlugin(
+            // if you have anymore problems tweet me at @gdi2290
+            // The (\\|\/) piece accounts for path separators for Windows and MacOS
+            /(.+)?angular(\\|\/)core(.+)?/,
+            path.join(__dirname, 'src'), // location of your src
+            {} // a map of your routes
+        )
+    ]
+};
+
+
